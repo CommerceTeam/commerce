6e61ffc35382e8038124ba1950b3831d2bfa7500
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-12-28 10:10:46 +0000

    * fixed Bug 6917, Thanks to Batomo
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/trunk@7696 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_commerce_pi1.php
+++ pi1/class.tx_commerce_pi1.php
@@ -367,7 +367,8 @@ class tx_commerce_pi1 extends tx_commerce_pibase {
 		 * TODO make it possible to have more than one link, to each of the productCategories
 		 */
 		$linkContent=$this->cObj->getSubpart($content,'###CATEGORY_ITEM_DETAILLINK###');
-		$link=$this->pi_linkTP($linkContent,array('catUid'=>$catObj->get_uid()),true);
+		$link=$this->pi_linkTP($linkContent,array('tx_commerce_pi1[catUid]'=>$catObj->get_uid()),true);
+		
 		$content=$this->cObj->substituteSubpart($content,'###CATEGORY_ITEM_DETAILLINK###',$link);
 	
 	
