e99d936c4991d58746bdc5da85bf6d9225157da5
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-20 19:04:03 +0000

    Fixed deprecated method call get_atrribute_matrix
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/trunk@30252 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_commerce_pibase.php
+++ lib/class.tx_commerce_pibase.php
@@ -210,7 +210,6 @@ class tx_commerce_pibase extends tslib_pibase {
 			$showHiddenValues = false;
 		}
 
-		#$matrix = $prodObj->get_atrribute_matrix('',$this->product_attributes,$showHiddenValues);
 		$matrix = $prodObj->get_product_attribute_matrix($this->product_attributes,$showHiddenValues);
 
 		$i = 0;
@@ -288,7 +287,7 @@ class tx_commerce_pibase extends tslib_pibase {
 		$this->shall_attributes = $prodObj->get_attributes(array(ATTRIB_shal));
 		
 		
-  		$matrix = $prodObj->get_atrribute_matrix($articleId,$this->shall_attributes,$showHiddenValues);
+  		$matrix = $prodObj->get_attribute_matrix($articleId,$this->shall_attributes,$showHiddenValues);
 	 	$i = 0;
 		if(is_array($this->shall_attributes)){
           	 foreach ($this->shall_attributes as $myAttributeUid) {
@@ -325,7 +324,7 @@ class tx_commerce_pibase extends tslib_pibase {
 
 		$article_shalAttributes_string = $this->cObj->stdWrap($article_shalAttributes_string,$this->conf['articleShalAttributsWrap.']) ;
 
-        $matrix = $prodObj->get_atrribute_matrix($articleId,$this->can_attributes,$showHiddenValues);
+        $matrix = $prodObj->get_attribute_matrix($articleId,$this->can_attributes,$showHiddenValues);
 		
 		$i = 0;
 		if(is_array($this->can_attributes)){
