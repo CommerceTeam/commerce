b389aa7fe8330e38808f4c5eb8fca3be59b9725b
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-04 23:16:49 +0000

    Add tx_commerce_category to autoloader
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/trunk@47313 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -5,6 +5,7 @@ $extensionPath = t3lib_extMgm::extPath('commerce');
 return array(
 	'tx_commerce_basket' => $extensionPath . 'lib/class.tx_commerce_basket.php',
 	'tx_commerce_basket_item' => $extensionPath . 'lib/class.tx_commerce_basket_item.php',
+	'tx_commerce_category' => $extensionPath . 'lib/class.tx_commerce_category.php',
 	'tx_commerce_div' => $extensionPath . 'lib/class.tx_commerce_div.php',
 	'tx_commerce_pibase' => $extensionPath . 'lib/class.tx_commerce_pibase.php',
 	'tx_commerce_product' => $extensionPath . 'lib/class.tx_commerce_product.php',
