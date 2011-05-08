ea5e7fbd72050caaf1f7853b91a580c3b7f6297b
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-08 15:31:03 +0000

    cleanup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/trunk@47434 735d13b6-9817-0410-8766-e36946ffe9aa

--- dao/class.address_object.php
+++ dao/class.address_object.php
@@ -40,23 +40,11 @@
 require_once(dirname(__FILE__).'/class.basic_dao.php');
 require_once(dirname(__FILE__).'/class.feuser_address_fieldmapper.php');
 
-
 class address_object extends basic_object {
 
- 	//var $name;
- 	//var $surname;
- 	//var $title;
- 	//var $address;
- 	//var $company;
- 	//var $city;
- 	//var $zip;
- 	//var $country;
- 	//var $phone;
- 	//var $email;
- 	//var $fax;
- 	var $tx_commerce_fe_user_id;
- 	var $tx_commerce_address_type_id;
- 	var $tx_commerce_is_main_address;
+	var $tx_commerce_fe_user_id;
+	var $tx_commerce_address_type_id;
+	var $tx_commerce_is_main_address;
 
 	function address_object() {
 		//add mapped fields to object
@@ -67,102 +55,56 @@ class address_object extends basic_object {
 		}
 	}
 
-    function getName() {
+	function getName() {
 		return $this->name;
-    }
+	}
 
 	function setName($name) {
 		$this->name = $name;
 	}
-
- }
-
- //-------------------- database access ----------------------
-
+}
 
 /**
-* address dao parser
-* This class is used by the dao for object/model parsing.
-* It extends the basic dao parser.
-*
-* @author Carsten Lausen <cl@e-netconsulting.de>
-*/
+ * address dao parser
+ * This class is used by the dao for object/model parsing.
+ * It extends the basic dao parser.
+ *
+ * @author Carsten Lausen <cl@e-netconsulting.de>
+ */
 class address_dao_parser extends basic_dao_parser {
-
-
 }
 
 
 /**
-* address dao mapping
-* This class used by the dao for database storage.
-* It extends the basic dao mapper.
-*
-* @author Carsten Lausen <cl@e-netconsulting.de>
-*/
+ * address dao mapping
+ * This class used by the dao for database storage.
+ * It extends the basic dao mapper.
+ *
+ * @author Carsten Lausen <cl@e-netconsulting.de>
+ */
 class address_dao_mapper extends basic_dao_mapper {
 
- 	function init() {
-	 	$this->dbTable = 'tt_address';	//dbtable for persistence
+	function init() {
+		$this->dbTable = 'tt_address';	//dbtable for persistence
 		$this->createPid = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][COMMERCE_EXTkey]['extConf']['create_address_pid'];   //new record pid
- 	}
-
-// 	function save(&$obj) {
-// 		if($obj->issetId()) {
-// 			$this->dbUpdate($obj->getId(),$obj);
-// 		} else {
-// 			$this->dbInsert($obj);
-// 		}
-//
-// 		//update main address flag if needed
-// 		if(($obj->tx_commerce_is_main_address) AND (!empty($obj->tx_commerce_fe_user_id))) {
-// 			$this->dbUpdateMainAddressFlag($obj->getId(),$obj->tx_commerce_fe_user_id);
-// 		}
-// 	}
-//
-// 	//---------- private db functions -------------
-//
-// 	function dbUpdateMainAddressFlag($uid, $feuser_id) {
-//
-// 		$dbTable = $this->dbTable;
-// 		$dbWhere = '(uid != "'.$uid.'")';
-// 		$dbWhere .= 'AND (tx_commerce_fe_user_id="'.$feuser_id.'")';
-// 		$dbModel = array('tx_commerce_is_main_address'=>'0');
-//
-//		//execute query
-////		debug($dbModel);
-////		debug(array('dbUpdate' => $GLOBALS['TYPO3_DB']->UPDATEquery($dbTable, $dbWhere, $dbModel)));
-//		$res = $GLOBALS['TYPO3_DB']->exec_UPDATEquery($dbTable, $dbWhere, $dbModel);
-//
-//		//any errors
-//		$error=$GLOBALS['TYPO3_DB']->sql_error();
-//		if(!empty($error)) $this->addError(array($error,$GLOBALS['TYPO3_DB']->UPDATEquery($dbTable, $dbWhere, $dbModel),'$dbModel' => $dbModel));
-
-
-// 	}
-
-
-
-
-
+	}
 }
 
 
 /**
-* address dao
-* This class handles object persistence using the dao design pattern.
-* It extends the basic dao object.
-*
-* @author Carsten Lausen <cl@e-netconsulting.de>
-*/
+ * address dao
+ * This class handles object persistence using the dao design pattern.
+ * It extends the basic dao object.
+ *
+ * @author Carsten Lausen <cl@e-netconsulting.de>
+ */
 class address_dao extends basic_dao {
 
- 	function init() {
- 		$this->parser =& new address_dao_parser();
- 		$this->mapper =& new address_dao_mapper($this->parser);
- 		$this->obj =& new address_object;
- 	}
-
+	function init() {
+		$this->parser =& new address_dao_parser();
+		$this->mapper =& new address_dao_mapper($this->parser);
+		$this->obj =& new address_object;
+	}
 }
 
 // Include extension?
