fe094e8ffe6c608f158b10ef7295e2a567f7e2a6
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-11 08:16:58 +0000

    CGL cleanup and method visibility in lib/class.tx_commerce_article_price.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/trunk@47557 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_commerce_article_price.php
+++ lib/class.tx_commerce_article_price.php
@@ -23,14 +23,7 @@
  ***************************************************************/
 
 /**
- * Libary for Frontend-Rendering of article prices. This class
- * should be used for all Fronten-Rendering, no Database calls
- * to the commerce tables should be made directly
- * This Class is inhertited from tx_commerce_element_alib, all
- * basic Database calls are made from a separate Database Class
- *
- * Do not acces class variables directly, allways use the get and set methods,
- * variables will be changed in php5 to private
+ * Libary for frontend rendering of article prices.
  *
  * @author Volker Graubaum <vg@e-netconsulting.de>
  * @coauthor Ingo Schmitt <is@marketing-factory.de>
@@ -40,207 +33,169 @@
 class tx_commerce_article_price extends tx_commerce_element_alib {
 
 	/**
-	 * @var currency for price, will move into shop system
+	 * @var String Currency for price
 	 */
-	var $currency = 'EUR';
+	protected $currency = 'EUR';
 
-	 /**
-	  * @Price Scae amount Start
-	  * @access private
-	  * @private
-	  */
-	 var $price_scale_amount_start=1;
-	 /**
-	  * @Price Scae amount End
-	  * @access private
-	  * @private
-	  */
-	  var $price_scale_amount_end=1;
-   
-   	 /**
-   	  * @var price gross
-   	  * @acces private
-   	  */
-	  var $price_gross=0;
-	   /**
-   	  * @var price net
-   	  * @acces private
-   	  */
-	  var $price_net=0;
-	 /**
-	  * Method called by constuctor
-	  * @param integer uid of product
-	  * @param integer integer language_uid , default 0
-	  */
-	  function init($uid,$lang_uid=0) {
-	    /**
-	    * Define variables
-	    */
-	    $uid = intval($uid);
-	    $lang_uid = intval($lang_uid);
-	    
-	     $this->database_class='tx_commerce_db_price';
-	     $this->fieldlist=array('price_net','price_gross','fe_group','price_scale_amount_start','price_scale_amount_end');
-	     if ($uid > 0){
-	              $this->uid=$uid;
-	              // Set Lang_uid always to 0, as there are no lokalised Versions of prices
-	              $this->lang_uid = 0;
-	              $this->conn_db=new $this->database_class;
-	              
-	              $hookObjectsArr = array();
-				  if (is_array ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postinit'])) {
-						foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postinit'] as $classRef) {
-							$hookObjectsArr[] = &t3lib_div::getUserObj($classRef);
-						}
-				  }
-				  foreach($hookObjectsArr as $hookObj)	{
-						if (method_exists($hookObj, 'postinit')) {
-							$hookObj->postinit($this);
-						}
-				  }
-	              
-	              return true;
-	     }else{
-		         return false;
-         }
-          
-     }
-     
 	/**
-	 * Constructor
-	 * 
-	 * @param integer uid of product
-	 * @param integer integer language_uid , default 0
-	 * @abstract calls init-Method
-	 * @depricated use make_instance and init of object
+	 * @var integer Price scale amount start
 	 */
-	 
-	 function tx_commerce_article_price() {   	
-	  	if ((func_num_args()>0) && (func_num_args()<=2)){
-			$uid = func_get_arg(0); 
-			if (func_num_args()==2){
-				$lang_uid=func_get_arg(1);
-			}else{
-				$lang_uid=0;
+	protected $price_scale_amount_start = 1;
+
+	/**
+	 * @var integer Price scale amount end
+	 */
+	protected $price_scale_amount_end = 1;
+
+	/**
+	 * @var integer Price gross
+	 */
+	protected $price_gross = 0;
+
+	/**
+	 * @var integer Price net
+	 */
+	protected $price_net = 0;
+
+	/**
+	 * Usual init method
+	 *
+	 * @param integer $uid Uid of product
+	 * @param integer $lang_uid Uid of language, unused
+	 * @return booloan TRUE if $uid is > 0
+	 */
+	public function init($uid, $lang_uid = 0) {
+
+		$this->database_class = 'tx_commerce_db_price';
+
+		$this->fieldlist = array(
+			'price_net',
+			'price_gross',
+			'fe_group',
+			'price_scale_amount_start',
+			'price_scale_amount_end'
+		);
+
+		$initializationResult = FALSE;
+		$uid = intval($uid);
+		if ($uid > 0) {
+			$this->uid = $uid;
+
+			$this->conn_db = new $this->database_class;
+
+			$hookObjectsArr = array();
+			if (is_array ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postinit'])) {
+				foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postinit'] as $classRef) {
+					$hookObjectsArr[] = t3lib_div::getUserObj($classRef);
+				}
 			}
-			return $this->init($uid,$lang_uid);
+			foreach($hookObjectsArr as $hookObj) {
+				if (method_exists($hookObj, 'postinit')) {
+					$hookObj->postinit($this);
+				}
+			}
+
+			$initializationResult = TRUE;
 		}
-	 }																																								      
-																																												      
+
+		return $initializationResult;
+	}
+
 	/**
-	 * Removed get_uid since inherited from element_alib
-	 * Removed return_assoc_array since inherited from element_alib
-	 * removed is_valid_uid since inherited from element_alib
+	 * Get net price
+	 *
+	 * @return integer Price net
 	 */
-
-	
-
-	
-	
-	
-	
-	 /**
-       * @return double priceNet 
-       * @access public
-       */
-	function getPriceNet(){
+	public function getPriceNet() {
 		$hookObjectsArr = array();
-		if (is_array ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postpricenet'])) {
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postpricenet'])) {
 			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postpricenet'] as $classRef) {
-				$hookObjectsArr[] = &t3lib_div::getUserObj($classRef);
+				$hookObjectsArr[] = t3lib_div::getUserObj($classRef);
 			}
 		}
-		foreach($hookObjectsArr as $hookObj)	{
+		foreach($hookObjectsArr as $hookObj) {
 			if (method_exists($hookObj, 'postpricenet')) {
 				$hookObj->postpricenet($this);
 			}
 		}
+
 		return $this->price_net;
-		
 	}
-	 /**
-       * @return double priceGross
-       * @access public
-       */
-	function getPriceGross(){
+
+	/**
+	 * Get price gross
+	 *
+	 * @return integer price gross
+	 */
+	public function getPriceGross() {
 		$hookObjectsArr = array();
-		if (is_array ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postpricegross'])) {
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postpricegross'])) {
 			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['commerce/lib/class.tx_commerce_article_price.php']['postpricegross'] as $classRef) {
-				$hookObjectsArr[] = &t3lib_div::getUserObj($classRef);
+				$hookObjectsArr[] = t3lib_div::getUserObj($classRef);
 			}
 		}
-		foreach($hookObjectsArr as $hookObj)	{
+		foreach($hookObjectsArr as $hookObj) {
 			if (method_exists($hookObj, 'postpricegross')) {
 				$hookObj->postpricegross($this);
 			}
 		}
+
 		return $this->price_gross;
-		
 	}
-	
+
 	/**
-	 * returns The Proce Scae Amount Start
-	 * @return integer 
+	 * Get price scale amount start
+	 *
+	 * @return integer Scale amount start
 	 */
-	
-	function getPriceScaleAmountStart(){
+	public function getPriceScaleAmountStart() {
 		return $this->price_scale_amount_start;
 	}
-	
+
 	/**
-	 * returns The Price Scale Amount End
-	 * @return integer 
+	 * Get price scale amount end
+	 *
+	 * @return integer Scale amount end
 	 */
-	function getPriceScaleAmountEnd(){
+	public function getPriceScaleAmountEnd() {
 		return $this->price_scale_amount_end;
 	}
- 	
+
 	/**
-	 * Returns the label for the TCA
-	 * Only for use in TCA
-	 * @params array	record value
-	 * @params object	Parent Object
-	 * @return array	new record values
+	 * Returns TCA label, used in TCA only
+	 *
+	 * @TODO: Move this method somewhere, it does not belong here
+	 * @params array Record value
+	 * @params object Parent Object
+	 * @return array New record values
 	 */
-	function getTCARecordTitle($params, $pObj){
-		global $LANG;
-		
-		$params['title'] = 
-			$LANG->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices','price_gross'),1).': '.tx_commerce_div::FormatPrice($params['row']['price_gross']/100).
-			' ,'.$LANG->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices','price_net'),1).': '.tx_commerce_div::FormatPrice($params['row']['price_net']/100).
-			' ('.$LANG->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices','price_scale_amount_start'),1).': '.$params['row']['price_scale_amount_start'].
-			'  '.$LANG->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices','price_scale_amount_end'),1).': '.$params['row']['price_scale_amount_end'].') '.
-			' '.($params['row']['fe_group'] ?( $LANG->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices','fe_group'),1).' '.t3lib_BEfunc::getProcessedValueExtra('tx_commerce_article_prices','fe_group',$params['row']['fe_group'],100,$params['row']['uid'])) : '')
-			;
+	public function getTCARecordTitle($params, $pObj) {
+		$params['title'] =
+			$GLOBALS['LANG']->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices', 'price_gross'), 1) . ': ' . tx_commerce_div::FormatPrice($params['row']['price_gross'] / 100) .
+			' ,' . $GLOBALS['LANG']->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices', 'price_net'), 1) . ': ' . tx_commerce_div::FormatPrice($params['row']['price_net']/100) .
+			' (' . $GLOBALS['LANG']->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices','price_scale_amount_start'),1) . ': ' . $params['row']['price_scale_amount_start'] .
+			'  ' . $GLOBALS['LANG']->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices', 'price_scale_amount_end'), 1) . ': ' . $params['row']['price_scale_amount_end'] . ') ' .
+			' ' . ($params['row']['fe_group'] ? ($GLOBALS['LANG']->sL(t3lib_befunc::getItemLabel('tx_commerce_article_prices', 'fe_group'), 1) . ' ' . t3lib_BEfunc::getProcessedValueExtra('tx_commerce_article_prices', 'fe_group', $params['row']['fe_group'], 100, $params['row']['uid'])) : '')
+		;
+
 		return $params;
 	}
- 	
- 	/**
-	 * Returns the net price as value
-	 * @return integer	netPrice
-	 * @deprecated
-	 * @see getPriceNet()
-	 */
-	function get_price_net(){
-	    return $this->getPriceNet();
-	}
-	
-	
+
 	/**
-	 * Returns the gross Price
-	 * @return 	integer	grossPrice
 	 * @deprecated
-	 * @see getPriceGross();
 	 */
-	
-	function get_price_gross(){
-	    return $this->getPriceGross();
+	public function get_price_net() {
+		return $this->getPriceNet();
 	}
-	
-	
- 	
- }
- 
+
+	/**
+	 * @deprecated
+	 */
+	public function get_price_gross() {
+		return $this->getPriceGross();
+	}
+}
+
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/commerce/lib/class.tx_commerce_article_price.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/commerce/lib/class.tx_commerce_article_price.php']);
 }
