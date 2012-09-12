463f0f0aee7794df8a9581477e0284223c5a900e
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-12 08:11:35 +0000

    Fixed 37954
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/trunk@65979 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_commerce_pi1.php
+++ pi1/class.tx_commerce_pi1.php
@@ -52,7 +52,7 @@ class tx_commerce_pi1 extends tx_commerce_pibase {
 		parent::init($conf);
 
 		// Merge default vars, if other prefix_id
-		if ($this->prefixID <> 'tx_commerce_pi1') {
+		if ($this->prefixId <> 'tx_commerce_pi1') {
 			$tx_commerce_vars = t3lib_div::_GP('tx_commerce');
 			if (is_array($tx_commerce_vars)) {
 				foreach($tx_commerce_vars as $key => $value) {
