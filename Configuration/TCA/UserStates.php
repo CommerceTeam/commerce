7c4232767feada5d7c2a35ea55b0bae2a9581963
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-21 08:17:30 +0000

    Split remaining tca definitions in own files and remove tca.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/branches/55612@84388 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TCA/UserStates.php
+++ Configuration/TCA/UserStates.php
@@ -1,79 +1,4 @@
 <?php
-/**
- * $Id: tca.php 308 2006-07-26 22:23:51Z ingo $
- */
-
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-
-$GLOBALS['TCA']['tx_commerce_address_types'] = Array(
-	'ctrl' => $GLOBALS['TCA']['tx_commerce_address_types']['ctrl'],
-	'interface' => Array(
-		'showRecordFieldList' => 'sys_language_uid,l18n_parent,l18n_diffsource,title'
-	),
-	'feInterface' => $GLOBALS['TCA']['tx_commerce_address_types']['feInterface'],
-	'columns' => Array(
-		'sys_language_uid' => Array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.php:LGL.language',
-			'config' => Array(
-				'type' => 'select',
-				'foreign_table' => 'sys_language',
-				'foreign_table_where' => 'ORDER BY sys_language.title',
-				'items' => Array(
-					Array('LLL:EXT:lang/locallang_general.php:LGL.allLanguages', -1),
-					Array('LLL:EXT:lang/locallang_general.php:LGL.default_value', 0)
-				)
-			)
-		),
-		'l18n_parent' => Array(
-			'displayCond' => 'FIELD:sys_language_uid:>:0',
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.php:LGL.l18n_parent',
-			'config' => Array(
-				'type' => 'select',
-				'items' => Array(
-					Array('', 0),
-				),
-				'foreign_table' => 'tx_commerce_address_types',
-				'foreign_table_where' => 'AND tx_commerce_address_types.pid=###CURRENT_PID### AND tx_commerce_address_types.sys_language_uid IN (-1,0)',
-			)
-		),
-		'l18n_diffsource' => Array(
-			'config' => Array(
-				'type' => 'passthrough'
-			)
-		),
-		'title' => Array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:commerce/Resources/Private/Language/locallang_db.xml:tx_commerce_address_types.title',
-			'config' => Array(
-				'type' => 'input',
-				'size' => '40',
-				'max' => '80',
-				'eval' => 'required,trim',
-			)
-		),
-		'name' => Array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:commerce/Resources/Private/Language/locallang_db.xml:tx_commerce_address_types.name',
-			'config' => Array(
-				'type' => 'input',
-				'size' => '40',
-				'max' => '80',
-				'eval' => 'required,trim',
-			)
-		),
-	),
-	'types' => Array(
-		'0' => Array('showitem' => 'sys_language_uid;;;;1-1-1, l18n_parent, l18n_diffsource, title;;;;2-2-2')
-	),
-	'palettes' => Array(
-		'1' => Array('showitem' => '')
-	)
-);
-
 
 $GLOBALS['TCA']['tx_commerce_user_states'] = Array(
 	'ctrl' => $GLOBALS['TCA']['tx_commerce_user_states']['ctrl'],
@@ -147,6 +72,5 @@ $GLOBALS['TCA']['tx_commerce_user_states'] = Array(
 		'1' => Array('showitem' => '')
 	)
 );
-
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -652,7 +652,7 @@ $GLOBALS['TCA']['tx_commerce_address_types'] = array(
 		'transOrigPointerField' => 'l18n_parent',
 		'transOrigDiffSourceField' => 'l18n_diffsource',
 		'default_sortby' => 'ORDER BY crdate',
-		'dynamicConfigFile' => PATH_TXCOMMERCE . 'tcafiles/tca.php',
+		'dynamicConfigFile' => PATH_TXCOMMERCE . 'tcafiles/AddressTypes.php',
 		'iconfile' => PATH_TXCOMMERCE_ICON_TABLE_REL . 'address_types.gif',
 	),
 	'feInterface' => array(
@@ -672,7 +672,7 @@ $GLOBALS['TCA']['tx_commerce_user_states'] = array(
 		'transOrigDiffSourceField' => 'l18n_diffsource',
 		'default_sortby' => 'ORDER BY crdate',
 		'delete' => 'deleted',
-		'dynamicConfigFile' => PATH_TXCOMMERCE . 'tcafiles/tca.php',
+		'dynamicConfigFile' => PATH_TXCOMMERCE . 'tcafiles/UserStates.php',
 		'iconfile' => PATH_TXCOMMERCE_ICON_TABLE_REL . 'user_states.gif',
 	),
 	'feInterface' => array(
