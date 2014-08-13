cee783f1b5d538205590ce09455d6cb00f031d17
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-08-13 11:39:29 +0000

    Rename marker with deprecation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/branches/55612@86164 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/BasketController.php
+++ Classes/Controller/BasketController.php
@@ -1013,7 +1013,9 @@ class Tx_Commerce_Controller_BasketController extends Tx_Commerce_Controller_Bas
 			'[artAddUid][' . $article->getUid() . '][price_id]=' . $basketItem->getPriceUid();
 		$typoLinkConf['additionalParams'] .= $this->argSeparator . $this->prefixId .
 			'[artAddUid][' . $article->getUid() . '][count]=0';
-		$markerArray['###DELIOTMFROMBASKETLINK###'] = $this->cObj->typoLink($this->pi_getLL('lang_basket_delete_item'), $typoLinkConf);
+		// DELIOTMFROMBASKETLINK is deprecated
+		$markerArray['###DELETEMFROMBASKETLINK###'] = $this->cObj->typoLink($this->pi_getLL('lang_basket_delete_item'), $typoLinkConf);
+		$markerArray['###DELIOTMFROMBASKETLINK###'] = $markerArray['###DELETEMFROMBASKETLINK###'];
 
 		$templateMarker = '###PRODUCT_BASKET_FORM_SMALL###';
 		$template = $this->cObj->getSubpart($this->templateCode, $templateMarker);
