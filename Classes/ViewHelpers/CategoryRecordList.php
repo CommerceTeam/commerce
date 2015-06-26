342c4318e7fb832c085683674ccc38377c1cf025
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-06-26 09:04:22 +0000

    [67597] Change translation fetching
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commerce/trunk@89300 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/CategoryRecordList.php
+++ Classes/ViewHelpers/CategoryRecordList.php
@@ -1504,13 +1504,14 @@ class Tx_Commerce_ViewHelpers_CategoryRecordList extends \TYPO3\CMS\Recordlist\R
 
 		$out = array(0 => '', 1 => '');
 
+		$this->translations = array();
 		$translations = $this->translateTools->translationInfo($table, $row['uid'], 0, $row, $this->selFieldList);
-		$this->translations = $translations['translations'];
 
 			// Language title and icon:
 		$out[0] = $this->languageFlag($row[$GLOBALS['TCA'][$table]['ctrl']['languageField']]);
 
 		if (is_array($translations)) {
+			$this->translations = $translations['translations'];
 			// Traverse page translations and add icon
 			// for each language that does NOT yet exist:
 			$lNew = '';
