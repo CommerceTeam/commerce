45ee550fa55a5115d0c173e793e75177ba9bf181
Sebastian Fischer <typo3@evoweb.de>
2017-04-04 09:41:19 +0200

    Refactor to use constructor pattern

--- Resources/Public/JavaScript/FormElementCategoryTree.js
+++ Resources/Public/JavaScript/FormElementCategoryTree.js
@@ -1,4 +1,4 @@
-/* globals window, TYPO3, TBE_EDITOR */
+/* globals TYPO3, TBE_EDITOR */
 
 /*
  * This file is part of the TYPO3 Commerce project.
@@ -10,212 +10,226 @@
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  */
-
-/**
- * Module: TYPO3/CMS/Commerce/FormElementCategoryTree
- * Javascript functions regarding the category tree in category element
- */
-define(['TYPO3/CMS/Backend/FormEngine', 'jquery'], function(FormEngine, $) {
+define(['require', 'TYPO3/CMS/Backend/FormEngine', 'jquery'], function(require, FormEngine, $) {
 	'use strict';
 
 	/** @var {Object} TYPO3 */
+	/** @var {{ajaxUrls: array}} settings */
 	var settings = TYPO3.settings,
 		ajaxUrl = settings.ajaxUrls['commerce_category_tree'];
 
-	var FormElementCategoryTree = {};
-
 	/**
-	 * Add clicked element to selectbox if not already added
+	 * Module: TYPO3/CMS/Commerce/FormElementCategoryTree
+	 * Enables interaction with category selection tree
 	 *
-	 * @param $listFieldEl
-	 * @param fieldName
-	 * @param $treeElement
+	 * @exports TYPO3/CMS/Backend/FormElementCategoryTree
 	 */
-	FormElementCategoryTree.addItemToSelectbox = function($listFieldEl, fieldName, $treeElement) {
-		// store clicked element for later comparison in remove option method
-		$listFieldEl.data('selected-values').push($treeElement.data('value'));
+	var FormElementCategoryTree = (function () {
+		/**
+		 * The constructor, set the class properties default values
+		 */
+		function FormElementCategoryTree() {
+			var _this = this;
 
-		// set element active and change background
-		$treeElement.parent().addClass('bg-success').parent().addClass('active');
+			this.treeItemClass = '.t3js-commerce-categorytree-itemstoselect';
+			this.removeItemClass = '.commerce-categorytree-element .t3js-btn-removeoption';
 
-		FormEngine.setSelectOptionFromExternalSource(
-			fieldName,
-			$treeElement.data('value'),
-			$treeElement.text(),
-			$treeElement.attr('title'),
-			''
-		);
-		TBE_EDITOR.fieldChanged();
-	};
+			/**
+			 * Add clicked element to selectbox if not already added
+			 *
+			 * @param $listFieldEl
+			 * @param fieldName
+			 * @param $treeElement
+			 */
+			this.addItemToSelectbox = function ($listFieldEl, $treeElement, fieldName) {
+				// store clicked element for later comparison in remove option method
+				$listFieldEl.data('selected-values').push($treeElement.data('value'));
 
-	/**
-	 * removes currently selected options from a select field
-	 *
-	 * @param {Object} $listFieldEl a jQuery object, containing the select field
-	 * @param {Object} $availableFieldEl a jQuery object, containing all available value
-	 */
-	FormElementCategoryTree.removeOption = function ($listFieldEl, $availableFieldEl) {
-		var previousSelected = $listFieldEl.data('selected-values');
+				// set element active and change background
+				$treeElement.parent().addClass('bg-success').parent().addClass('active');
 
-		$.each(previousSelected, function (i, previousValue) {
-			var stillSelected = $listFieldEl.find('option[value="' + previousValue + '"]:not(:selected)').length > 0;
+				FormEngine.setSelectOptionFromExternalSource(
+					fieldName,
+					$treeElement.data('value'),
+					$treeElement.text(),
+					$treeElement.attr('title'),
+					''
+				);
+				TBE_EDITOR.fieldChanged();
+			};
 
-			if (!stillSelected) {
-				$availableFieldEl
-					.find('[data-value="' + previousValue + '"]')
-						.parent()
-						.removeClass('bg-success')
+			/**
+			 * removes currently selected options from a select field
+			 *
+			 * @param {Object} $listFieldEl a jQuery object, containing the select field
+			 * @param {Object} $availableFieldEl a jQuery object, containing all available value
+			 */
+			this.removeOption = function ($listFieldEl, $availableFieldEl) {
+				var previousSelected = $listFieldEl.data('selected-values');
+
+				$.each(previousSelected, function (i, previousValue) {
+					var stillSelected = $listFieldEl.find('option[value="' + previousValue + '"]:not(:selected)').length > 0;
+
+					if (!stillSelected) {
+						$availableFieldEl
+							.find('[data-value="' + previousValue + '"]')
+							.parent()
+							.removeClass('bg-success')
 							.parent()
 							.removeClass('active');
-			}
-		});
-	};
+					}
+				});
+			};
 
-	/**
-	 * Open or close subtree based on element state
-	 *
-	 * @param $listFieldEl
-	 * @param $treeElement
-	 */
-	FormElementCategoryTree.openOrCloseSubtree = function($listFieldEl, $treeElement) {
-		var $parent = $treeElement.parent().parent(),
-			$submenu = $parent.find('> ul');
+			/**
+			 * Open or close subtree based on element state
+			 *
+			 * @param $listFieldEl
+			 * @param $treeElement
+			 */
+			this.openOrCloseSubtree = function ($listFieldEl, $treeElement) {
+				var $parent = $treeElement.parent().parent(),
+					$submenu = $parent.find('> ul');
 
-		if ($treeElement.hasClass('list-tree-control-open')) {
-			FormElementCategoryTree.closeSubtree($treeElement, $parent, $submenu);
-		} else {
-			FormElementCategoryTree.openSubtree($listFieldEl, $treeElement, $parent, $submenu);
+				if ($treeElement.hasClass('list-tree-control-open')) {
+					this.closeSubtree($treeElement, $parent, $submenu);
+				} else {
+					this.openSubtree($listFieldEl, $treeElement, $parent, $submenu);
+				}
+			};
+
+			/**
+			 * Open subtree if already loaded and closed or load subtree via ajax
+			 *
+			 * @param $listFieldEl
+			 * @param $treeElement
+			 * @param $parent
+			 * @param $submenu
+			 */
+			this.openSubtree = function ($listFieldEl, $treeElement, $parent, $submenu) {
+				$treeElement.removeClass('list-tree-control-closed').addClass('list-tree-control-open');
+				$treeElement.data('pm', $treeElement.data('pm').replace('0_0_', '0_1_'));
+				$parent.addClass('list-tree-control-open');
+
+				if ($submenu.length > 0) {
+					$submenu.css('display', 'inherit');
+
+					this.storeTreeState($treeElement);
+				} else {
+					this.getSubtree($listFieldEl, $treeElement);
+				}
+			};
+
+			/**
+			 * Load subtree with ajax
+			 *
+			 * @param $listFieldEl
+			 * @param $treeElement
+			 */
+			this.getSubtree = function ($listFieldEl, $treeElement) {
+				$.ajax({
+					url: ajaxUrl,
+					type: 'post',
+					dataType: 'html',
+					cache: false,
+					data: {
+						'action': 'getSubtree',
+						'PM': $treeElement.data('pm'),
+						'selectedItems': $listFieldEl.children('option').map(function() { return $(this).val(); }).get()
+					}
+				}).done(function(response) {
+					$treeElement.parent().after(response);
+				});
+			};
+
+			/**
+			 * Close subtree
+			 *
+			 * @param $treeElement
+			 * @param $parent
+			 * @param $submenu
+			 */
+			this.closeSubtree = function ($treeElement, $parent, $submenu) {
+				$treeElement.removeClass('list-tree-control-open').addClass('list-tree-control-closed');
+				$treeElement.data('pm', $treeElement.data('pm').replace('0_1_', '0_0_'));
+				$parent.removeClass('list-tree-control-open');
+				$submenu.css('display', 'none');
+
+				this.storeTreeState($treeElement);
+			};
+
+			/**
+			 * Store elements state if an subtree was closed
+			 * The open state is saved while opening the subtree
+			 *
+			 * @param $treeElement
+			 */
+			this.storeTreeState = function ($treeElement) {
+				$.ajax({
+					url: ajaxUrl,
+					type: 'post',
+					dataType: 'html',
+					cache: false,
+					data: {
+						action: 'storeState',
+						PM: $treeElement.data('pm')
+					}
+				}).done(function() {
+					console.log('stored tree state');
+				});
+			};
+
+			/**
+			 * Handles event if a tree item was clicked
+			 *
+			 * @param event
+			 */
+			this.treeItemClicked = function (event) {
+				var $el = $(this),
+					fieldName = $el.data('relatedfieldname');
+
+				if (fieldName) {
+					var $listFieldEl = FormEngine.getFieldElement(fieldName, '_list', true),
+						$treeElement = $(event.target);
+
+					if ($treeElement.hasClass('list-tree-title')) {
+						_this.addItemToSelectbox($listFieldEl, $treeElement, fieldName);
+					} else if ($treeElement.hasClass('list-tree-control')) {
+						_this.openOrCloseSubtree($listFieldEl, $treeElement);
+					}
+				}
+			};
+
+			/**
+			 * Handles the event if the delete button to remove selected options was clicked
+			 */
+			this.removeItemClicked = function () {
+				var $el = $(this),
+					fieldName = $el.data('fieldname'),
+					$listFieldEl = FormEngine.getFieldElement(fieldName, '_list', true),
+					$availableFieldEl = $(
+						'div[data-relatedfieldname="' + fieldName + '"]',
+						FormEngine.getFormElement('')
+					);
+
+				_this.removeOption($listFieldEl, $availableFieldEl);
+			};
+
+			this.initialize();
 		}
-	};
 
-	/**
-	 * Open subtree if already loaded and closed or load subtree via ajax
-	 *
-	 * @param $listFieldEl
-	 * @param $treeElement
-	 * @param $parent
-	 * @param $submenu
-	 */
-	FormElementCategoryTree.openSubtree = function($listFieldEl, $treeElement, $parent, $submenu) {
-		$treeElement.removeClass('list-tree-control-closed').addClass('list-tree-control-open');
-		$treeElement.data('pm', $treeElement.data('pm').replace('0_0_', '0_1_'));
-		$parent.addClass('list-tree-control-open');
+		/**
+		 * initializes events using deferred bound to document
+		 * so AJAX reloads are no problem
+		 */
+		FormElementCategoryTree.prototype.initialize = function () {
+			var $document = $(document);
+			$document.on('click', this.treeItemClass, this.treeItemClicked);
+			$document.on('click', this.removeItemClass, this.removeItemClicked);
+		};
 
-		if ($submenu.length > 0) {
-			$submenu.css('display', 'inherit');
+		return FormElementCategoryTree;
+	}());
 
-			FormElementCategoryTree.storeTreeState($treeElement);
-		} else {
-			FormElementCategoryTree.getSubtree($listFieldEl, $treeElement);
-		}
-	};
-
-	/**
-	 * Load subtree with ajax
-	 *
-	 * @param $listFieldEl
-	 * @param $treeElement
-	 */
-	FormElementCategoryTree.getSubtree = function($listFieldEl, $treeElement) {
-		$.ajax({
-			url: ajaxUrl,
-			type: 'post',
-			dataType: 'html',
-			cache: false,
-			data: {
-				'action': 'getSubtree',
-				PM: $treeElement.data('pm'),
-				selectedItems: $listFieldEl.children('option').map(function() { return $(this).val(); }).get()
-			}
-		}).done(function(response) {
-			$treeElement.parent().after(response);
-		});
-	};
-
-	/**
-	 * Close subtree
-	 *
-	 * @param $treeElement
-	 * @param $parent
-	 * @param $submenu
-	 */
-	FormElementCategoryTree.closeSubtree = function($treeElement, $parent, $submenu) {
-		$treeElement.removeClass('list-tree-control-open').addClass('list-tree-control-closed');
-		$treeElement.data('pm', $treeElement.data('pm').replace('0_1_', '0_0_'));
-		$parent.removeClass('list-tree-control-open');
-		$submenu.css('display', 'none');
-
-		FormElementCategoryTree.storeTreeState($treeElement);
-	};
-
-	/**
-	 * Store elements state if an subtree was closed
-	 * The open state is saved while opening the subtree
-	 *
-	 * @param $treeElement
-	 */
-	FormElementCategoryTree.storeTreeState = function($treeElement) {
-		$.ajax({
-			url: ajaxUrl,
-			type: 'post',
-			dataType: 'html',
-			cache: false,
-			data: {
-				action: 'storeState',
-				PM: $treeElement.data('pm')
-			}
-		}).done(function() {
-			console.log('stored tree state');
-		});
-	};
-
-	FormElementCategoryTree.treeItemClicked = function (evt) {
-		var $el = $(this),
-			fieldName = $el.data('relatedfieldname');
-
-		if (fieldName) {
-			var $listFieldEl = FormEngine.getFieldElement(fieldName, '_list', true),
-				$treeElement = $(evt.target);
-
-			if ($treeElement.hasClass('list-tree-title')) {
-				FormElementCategoryTree.addItemToSelectbox($listFieldEl, fieldName, $treeElement);
-			} else if ($treeElement.hasClass('list-tree-control')) {
-				FormElementCategoryTree.openOrCloseSubtree($listFieldEl, $treeElement);
-			}
-		}
-	};
-
-	FormElementCategoryTree.removeItemClicked = function removeOption () {
-		var $el = $(this),
-			fieldName = $el.data('fieldname'),
-			$listFieldEl = FormEngine.getFieldElement(fieldName, '_list', true),
-			$availableFieldEl = $(
-				'div[data-relatedfieldname="' + fieldName + '"]',
-				FormEngine.getFormElement('')
-			);
-
-		FormElementCategoryTree.removeOption($listFieldEl, $availableFieldEl);
-	};
-
-	/**
-	 * initializes events using deferred bound to document
-	 * so AJAX reloads are no problem
-	 */
-	FormElementCategoryTree.initializeEvents = function() {
-		var $document = $(document);
-		$document.on(
-			'click',
-			'.t3js-commerce-categorytree-itemstoselect',
-			FormElementCategoryTree.treeItemClicked
-		);
-		$document.on(
-			'click',
-			'.commerce-categorytree-element .t3js-btn-removeoption',
-			FormElementCategoryTree.removeItemClicked
-		);
-	};
-
-	$(FormElementCategoryTree.initializeEvents);
-
-	return FormElementCategoryTree;
+	return new FormElementCategoryTree();
 });
