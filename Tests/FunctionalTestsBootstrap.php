8c9da87430cc96fa7e6f1e01bda977ad445add0d
Sebastian Fischer <typo3@evoweb.de>
2017-05-13 16:05:18 +0200

    Change base test class include

--- Tests/FunctionalTestsBootstrap.php
+++ Tests/FunctionalTestsBootstrap.php
@@ -5,7 +5,7 @@ if (!defined('PHPUNIT_COMPOSER_INSTALL')) {
     define('PHPUNIT_COMPOSER_INSTALL', $_SERVER['IDE_PHPUNIT_CUSTOM_LOADER']);
 }
 
-$file = realpath(__DIR__ . '/../../../../typo3_src/components/testing_framework/Resources/Core/Build/FunctionalTestsBootstrap.php');
+$file = realpath(__DIR__ . '/../../../../typo3_src/vendor/typo3/testing-framework/Resources/Core/Build/FunctionalTestsBootstrap.php');
 /** @noinspection PhpIncludeInspection */
 require_once $file;
 
